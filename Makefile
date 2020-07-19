d23f237a763b8c20c575b1b9cdb4f654c119d0bc
Alec M. Wantoch <alec@wantoch.com>
2020-07-18 22:34:48 -0400

    setup building ABIs into lib

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SHELL=/bin/bash
 
-# builds frontend to ui/public (will build lib first)
+# builds frontend to ui/public
 ui:
 	cd ui && npm run build
 
@@ -12,10 +12,20 @@ lib:
 dev: lib
 	cd ui && npm run develop
 
+# compile contracts
+contracts:
+	truffle build
+
 # migrates/deploys Solidity contracts via Truffle
 migrate:
 	truffle migrate
 
 deploy: migrate
 
-.PHONY: ui lib
+# build frontend
+frontend: lib ui
+
+# build all artifacts
+all: contracts lib ui
+
+.PHONY: ui lib contracts
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -25,6 +26,8 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
+  // Build ABIs to lib
+  contracts_build_directory: path.join(__dirname, "lib/abis"),
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
