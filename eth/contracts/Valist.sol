51fd05a6579c01938ebf63b2434eccee898948d2
Alec M. Wantoch <alec@wantoch.com>
2020-07-29 20:40:54 -0400

    add contract draft, vscode integration

--- eth/contracts/Valist.sol
+++ eth/contracts/Valist.sol
@@ -4,4 +4,28 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Valist is AccessControl {
 
+    bytes32 public constant APP_OWNER = keccak256("APP_OWNER_ROLE");
+    bytes32 public constant ORG_OWNER = keccak256("ORG_OWNER_ROLE");
+
+    bytes32 public constant APP_ADMIN = keccak256("APP_ADMIN_ROLE");
+    bytes32 public constant ORG_ADMIN = keccak256("ORG_ADMIN_ROLE");
+
+    bytes32 public constant APP_DEV = keccak256("APP_DEV_ROLE");
+
+    struct Repository {
+        mapping(bytes32 => address[]) usersByRole;
+        string name;
+        bytes32 id;
+    }
+
+    struct Organization {
+        mapping(bytes32 => address[]) usersByRole;
+        string name;
+        bytes32 id;
+    }
+
+    mapping(string => Repository[]) repos;
+
+    mapping(string => Organization) organization;
+
 }
