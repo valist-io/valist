2daf4c20e55343c0e3325fdd76961b42970629cb
Alec M. Wantoch <alec@wantoch.com>
2020-08-01 00:10:02 -0400

    improve access control

--- eth/contracts/ValistOrganization.sol
+++ eth/contracts/ValistOrganization.sol
@@ -5,31 +5,39 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./ValistRepository.sol";
 
 contract ValistOrganization is AccessControl {
-    bytes32 constant ORG_OWNER = keccak256("ORG_OWNER_ROLE");
+
     bytes32 constant ORG_ADMIN = keccak256("ORG_ADMIN_ROLE");
 
     string public meta; // org metadata (full name, image, description, etc)
 
+    event MetaUpdate(string meta);
+
     mapping(string => ValistRepository) public repos;
 
     modifier admin() {
-        require(hasRole(ORG_OWNER, msg.sender) || hasRole(ORG_ADMIN, msg.sender), "You do not have permission to modify this organization!");
+        require(hasRole(ORG_ADMIN, msg.sender), "You do not have permission to perform this action!");
         _;
     }
 
     constructor(address _owner, string memory _meta) public {
-        _setupRole(ORG_OWNER, _owner);
+        _setupRole(ORG_ADMIN, _owner);
+        _setRoleAdmin(ORG_ADMIN, ORG_ADMIN);
+
         meta = _meta;
     }
 
-    function createRepository(address _owner, string memory _repoName, string memory _meta) public returns(address) {
+    function createRepository(address _owner, string memory _repoName, string memory _meta) public admin returns(address) {
         require(address(repos[_repoName]) == address(0), "Repository already exists!");
+
         repos[_repoName] = new ValistRepository(_owner, _meta);
+
         return address(repos[_repoName]);
     }
 
     function updateOrgMeta(string memory _meta) public admin returns (string memory) {
         meta = _meta;
+
+        emit MetaUpdate(meta);
     }
 
 }
--- eth/contracts/ValistRepository.sol
+++ eth/contracts/ValistRepository.sol
@@ -4,45 +4,47 @@ pragma solidity >=0.4.21 <0.7.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract ValistRepository is AccessControl {
-    bytes32 constant REPO_OWNER = keccak256("REPO_OWNER_ROLE");
+
     bytes32 constant REPO_ADMIN = keccak256("REPO_ADMIN_ROLE");
     bytes32 constant REPO_DEV = keccak256("REPO_DEV_ROLE");
 
     string public meta; // ipfs URI for metadata (image, description, etc)
     string public latestRelease; // latest release hash (should equal releases[-1])
 
-    string[] public changelog; // list of IPFS uris for any changelogs (also emitted as an event during update)
     string[] public releases; // list of previous release ipfs hashes
 
-    event Release(string changelog, string release);
+    event Release(string release, string changelog);
+
+    event MetaUpdate(string meta);
 
     modifier admin() {
-      require(hasRole(REPO_OWNER, msg.sender) || hasRole(REPO_ADMIN, msg.sender), "You do not have permission to modify this repository!");
+      require(hasRole(REPO_ADMIN, msg.sender), "You do not have permission to perform this action!");
       _;
     }
 
-    modifier developers() {
-      require(hasRole(REPO_OWNER, msg.sender) ||
-              hasRole(REPO_ADMIN, msg.sender) ||
-              hasRole(REPO_DEV, msg.sender),
-              "You do not have permission to modify this repository!");
+    modifier developer() {
+      require(hasRole(REPO_ADMIN, msg.sender) || hasRole(REPO_DEV, msg.sender), "You do not have permission to perform this action!");
       _;
     }
 
     constructor(address _owner, string memory _meta) public {
-        _setupRole(REPO_OWNER, _owner);
+        _setupRole(REPO_ADMIN, _owner);
+        _setRoleAdmin(REPO_ADMIN, REPO_ADMIN);
+        _setRoleAdmin(REPO_DEV, REPO_ADMIN);
+
         meta = _meta;
     }
 
     function updateRepoMeta(string memory _meta) public admin returns (string memory) {
         meta = _meta;
+
+        emit MetaUpdate(_meta);
     }
 
-    function publishUpdate(string memory _changelog, string memory _release) public admin developers {
-        changelog.push(_changelog);
+    function publishRelease(string memory _changelog, string memory _release) public developer {
         releases.push(_release);
 
-        emit Release(_changelog, _release);
+        emit Release(_release, _changelog);
     }
 
 }
