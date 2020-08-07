a176f42c3bdf5cfc44ca66e0b0e8f90989f4138f
Zachary Pelkey <zpelkey@gmail.com>
2020-08-06 20:43:24 -0400

    Add intial create organization styles

--- relay/components/CreateOrganization/CreateOrganizationForm.tsx
+++ relay/components/CreateOrganization/CreateOrganizationForm.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useState, useEffect } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -52,15 +53,19 @@ export const CreateOrganizationForm:FunctionComponent<any> = ({valist}) => {
     }
 
     return (
-        <form className={classes.root} noValidate autoComplete="off">
-            <TextField onChange={(e) => setOrgShortName(e.target.value)} id="outlined-basic" label="Organization Short Name" variant="outlined" value={orgShortName}/>
-            <br></br>
-            <TextField onChange={(e) => setOrgFullName(e.target.value)} id="outlined-basic" label="Organization Full Name" variant="outlined" value={orgFullName} />
-            <br></br>
-            <TextField onChange={(e) => setOrgDescription(e.target.value)} id="outlined-basic" label="Description" variant="outlined" value={orgDescription} />
-            <br></br>
-            <Button onClick={createOrganization} value="Submit" variant="contained" color="primary">Create</Button>
-        </form>
+        <div id="org-card">
+            <div className="org-image" ></div>
+            <Card>
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField onChange={(e) => setOrgShortName(e.target.value)} id="outlined-basic" label="Organization Short Name" variant="outlined" value={orgShortName}/>
+                    <br></br>
+                    <TextField onChange={(e) => setOrgFullName(e.target.value)} id="outlined-basic" label="Organization Full Name" variant="outlined" value={orgFullName} />
+                    <br></br>
+                    <TextField onChange={(e) => setOrgDescription(e.target.value)} id="outlined-basic" label="Description" variant="outlined" value={orgDescription} />
+                    <br></br>
+                    <Button onClick={createOrganization} value="Submit" variant="contained" color="primary">Create</Button>
+                    </form>
+            </Card>
+        </div>
     );
-
 }
