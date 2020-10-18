575ba39f65962c111f5890ba8b92811d90c12be8
Zachary Pelkey <zpelkey@gmail.com>
2020-10-17 20:14:40 -0400

    Added key to activity component & fix create org link

--- relay/components/ActivityFeed/ActivityFeed.tsx
+++ relay/components/ActivityFeed/ActivityFeed.tsx
@@ -35,8 +35,8 @@ export const ActivityFeed:FunctionComponent<any> = ({}) => {
                     <h2 className="text-sm leading-5 font-semibold">Activity</h2>
                 </div>
                 <div>
-                {updates.map((update) => (
-                    <ul className="divide-y divide-gray-200">
+                {updates.map((update, index) => (
+                    <ul key={index} className="divide-y divide-gray-200">
                         <li className="py-4">
                         <div className="flex space-x-3">
                             <img className="h-6 w-6 rounded-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&h=256&q=80" alt="" />
--- relay/components/ProfileSidebar/ProfileSidebar.tsx
+++ relay/components/ProfileSidebar/ProfileSidebar.tsx
@@ -31,7 +31,7 @@ export const ProfileSidebar:FunctionComponent<any> = ({}) => {
                             </Link>
                         </span>
                         <span className="inline-flex rounded-md shadow-sm">
-                        <Link href="/repo/create">
+                        <Link href="/org/create">
                             <button type="button" className="w-full inline-flex items-center justify-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
                                 Create Organization
                             </button>
