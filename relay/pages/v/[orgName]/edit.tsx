d702b3f69687cf6831d162a5317fab522068db5b
Alec M. Wantoch <alec@wantoch.com>
2020-11-15 22:47:25 -0500

    add empty edit org page

---
+++ relay/pages/v/[orgName]/edit.tsx
@@ -0,0 +1,12 @@
+import Layout from '../../../components/Layout/Layout';
+import EditOrgForm from '../../../components/EditOrgForm/EditOrgForm';
+
+export const EditOrgPage = () => {
+    return (
+        <Layout title="Valist | Create Organization">
+            <EditOrgForm />
+        </Layout>
+    );
+}
+
+export default EditOrgPage;
