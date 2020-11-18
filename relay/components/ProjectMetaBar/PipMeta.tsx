96af9a69a52c804ecceaeeb37ae7fceda4a06758
valist-dev <74028032+valist-dev@users.noreply.github.com>
2020-11-18 00:21:18 -0500

    copy paste support

--- relay/components/ProjectMetaBar/PipMeta.tsx
+++ relay/components/ProjectMetaBar/PipMeta.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+
+import { copyToCB } from '../../utils/clipboard';
 
 const PipMeta = (orgName: string = "organization", repoName: string = "repo") => {
+
+    const pipRef = useRef(null);
 
     const [origin, setOrigin] = useState("https://app.valist.io");
     useEffect(() => {
@@ -13,7 +17,7 @@ const PipMeta = (orgName: string = "organization", repoName: string = "repo") =>
                 <div className="pt-6 pb-2">
                     <h1 className="flex-1 text-lg leading-7 font-medium">Pip Install From Url</h1>
                 </div>
-                <div className="border-2 border-solid border-black-200 rounded-lg p-2 bg-gray-200">
+                <div ref={pipRef} onClick={() => copyToCB(pipRef)} className="border-2 border-solid border-black-200 rounded-lg p-2 bg-gray-200 cursor-pointer">
                     pip install {origin}/api/{orgName}/{repoName}/latest
                 </div>
             </div>
