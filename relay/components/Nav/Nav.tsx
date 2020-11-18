2d92cfdfbda318c55b1d7f485d05f3e40a90cf87
Alec M. Wantoch <alec@wantoch.com>
2020-11-18 12:54:26 -0500

    remove search from mailbar, add temp mailto support link

--- relay/components/Nav/Nav.tsx
+++ relay/components/Nav/Nav.tsx
@@ -13,19 +13,7 @@ export const Nav:FunctionComponent<any> = ({}) => {
                             </div>
                         </Link>
                     </div>
-                    <div className="flex-1 flex justify-center lg:justify-end">
-                        <div className="w-full px-2 lg:px-6">
-                            <label className="sr-only">Search projects</label>
-                            <div className="relative text-indigo-300 focus-within:text-gray-400">
-                                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                    <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-                                    </svg>
-                                </div>
-                                <input id="search" className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-indigo-400 bg-opacity-25 text-indigo-300 placeholder-indigo-300 focus:outline-none focus:bg-white focus:placeholder-gray-400 focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out" placeholder="Search projects" type="search" />
-                            </div>
-                        </div>
-                    </div>
+
                     <div className="flex lg:hidden">
                         <button className="inline-flex items-center justify-center p-2 rounded-md text-indigo-400 hover:text-white hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
                             <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -40,7 +28,7 @@ export const Nav:FunctionComponent<any> = ({}) => {
                     <div className="flex items-center justify-end">
                         <div className="flex">
                         <a href="https://github.com/valist-io/valist" className="px-3 py-2 rounded-md text-sm leading-5 font-medium text-indigo-200 hover:text-white focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out">Documentation</a>
-                        <a href="https://valist.io" className="px-3 py-2 rounded-md text-sm leading-5 font-medium text-indigo-200 hover:text-white focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out">Support</a>
+                        <a href="mailto:support@valist.io?subject=Valist Support" className="px-3 py-2 rounded-md text-sm leading-5 font-medium text-indigo-200 hover:text-white focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out">Support</a>
                         </div>
 
                         <div className="ml-4 relative flex-shrink-0">
