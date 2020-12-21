b055d055851d3ac0a802fb746101658dea894018
Alec M. Wantoch <alec@wantoch.com>
2020-12-21 13:02:45 -0500

    deploy to kovan

--- eth/truffle-config.js
+++ eth/truffle-config.js
@@ -70,6 +70,11 @@ module.exports = {
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/`),
+      network_id: 42
+    },
+
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
