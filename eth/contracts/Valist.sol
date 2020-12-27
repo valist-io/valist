688793e4f59b1384327528c36f3cd8f0ec0bd085
Alec M. Wantoch <alec@wantoch.com>
2020-12-26 20:23:46 -0800

    rehaul contract logic for various UX & DX improvements

--- eth/contracts/Valist.sol
+++ eth/contracts/Valist.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.7.0;
+pragma experimental ABIEncoderV2;
 
 import "./ValistOrganization.sol";
 
@@ -7,33 +8,44 @@ contract Valist {
 
     bytes32 constant ORG_ADMIN = keccak256("ORG_ADMIN_ROLE");
 
-    // map an organization/username handle to an Organization contract
-    // [valist.io]/[organization]/[repository]
-    mapping(string => ValistOrganization) public orgs;
-
-    event OrganizationCreated(string orgName, string orgMeta);
-
-    event OrganizationDeleted(string orgName);
-
-    // register organization/username to the global valist namespace
-    function createOrganization(string memory orgName, string memory orgMeta) public returns(address) {
-        require(address(orgs[orgName]) == address(0), "Organization exists");
-
-        orgs[orgName] = new ValistOrganization(msg.sender, orgMeta);
-
-        emit OrganizationCreated(orgName, orgMeta);
-
-        return address(orgs[orgName]);
+    struct Organization {
+        uint index;
+        ValistOrganization org;
     }
 
-    function deleteOrganization(string memory orgName) public {
-        require(orgs[orgName].hasRole(ORG_ADMIN, msg.sender), "Access Denied");
+    // map an organization/username handle to an Organization contract
+    // this enables the following schema:
+    // [valist.io/other relays]/[organization]/[repository]
+    mapping(string => Organization) public orgs;
 
-        orgs[orgName]._deleteOrganization(msg.sender);
+    string[] public orgNames;
 
-        delete orgs[orgName];
+    function getOrgNames() public view returns (string[] memory) {
+        return orgNames;
+    }
 
-        emit OrganizationDeleted(orgName);
+    // register organization/username to the global valist namespace
+    function createOrganization(string memory _orgName, string memory _orgMeta) public returns(address) {
+        require(address(orgs[_orgName].org) == address(0), "Organization exists");
+
+        orgNames.push(_orgName);
+
+        orgs[_orgName].index = orgNames.length - 1;
+
+        orgs[_orgName].org = new ValistOrganization(msg.sender, _orgMeta);
+
+        return address(orgs[_orgName].org);
+    }
+
+    function deleteOrganization(string memory _orgName) public {
+        require(orgs[_orgName].org.hasRole(ORG_ADMIN, msg.sender), "Access Denied");
+
+        ValistOrganization org = orgs[_orgName].org;
+
+        delete orgNames[orgs[_orgName].index];
+        delete orgs[_orgName];
+
+        org._deleteOrganization(msg.sender);
     }
 
 }
