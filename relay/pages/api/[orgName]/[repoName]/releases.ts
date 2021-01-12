7d9ec4bd82ee9e7594b8f7af934a77673abb01f4
Alec M. Wantoch <alec@wantoch.com>
2021-01-12 15:54:37 -0500

    reverse order of releases array (latest is index 0)

--- relay/pages/api/[orgName]/[repoName]/releases.ts
+++ relay/pages/api/[orgName]/[repoName]/releases.ts
@@ -16,7 +16,7 @@ export default async function getReleasesFromRepo(req: NextApiRequest, res: Next
     const releases = await valist.getReleasesFromRepo(orgName.toString(), repoName.toString());
 
     if (releases) {
-      return res.status(200).json(releases);
+      return res.status(200).json(releases.reverse());
     } else {
       return res.status(404).json({statusCode: 404, message: "No releases found!"});
     }
