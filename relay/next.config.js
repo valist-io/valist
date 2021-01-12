bdbe6e0cccb386123c09070cb193de3338715ace
Alec M. Wantoch <alec@wantoch.com>
2021-01-12 15:14:35 -0500

    add CORS to API endpoints

--- relay/next.config.js
+++ relay/next.config.js
@@ -3,4 +3,15 @@ module.exports = {
     WEB3_PROVIDER: process.env.WEB3_PROVIDER || "http://127.0.0.1:9545",
     MAGIC_PUBKEY: "pk_test_54C6079CBEF87272",
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }
+        ]
+      }
+    ]
+  },
 }
