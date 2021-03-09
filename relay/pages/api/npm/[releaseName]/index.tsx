433586ed8d03330d67acfe5d1fa49a712900dbba
valist-dev <zach@valist.io>
2021-03-09 11:14:12 -0500

    Added basic npm install forwarding to npmjs registry .

--- relay/pages/api/npm/[releaseName]/index.tsx
+++ relay/pages/api/npm/[releaseName]/index.tsx
@@ -2,8 +2,9 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import Valist, { Web3Providers } from 'valist';
 
 export default async function getReleasesFromRepo(req: NextApiRequest, res: NextApiResponse) {
-
-  // set .env.local to your local chain or set in production deployment
+    console.log("Pulling the package list");
+  
+    // set .env.local to your local chain or set in production deployment
     if (process.env.WEB3_PROVIDER) {
 
         const valist = new Valist({ web3Provider: new Web3Providers.HttpProvider(process.env.WEB3_PROVIDER), metaTx: false });
@@ -12,38 +13,44 @@ export default async function getReleasesFromRepo(req: NextApiRequest, res: Next
         const {
             query: { releaseName },
         } = req;
+        
 
         const [orgName, repoName] = decodeURIComponent(releaseName.toString().replace("@", "")).split("/");
-        const releases = await valist.getReleasesFromRepo(orgName.toString(), repoName.toString());
 
-        const latestTag = await valist.getLatestTagFromRepo(orgName, repoName);
-        let versions: any = {};
+        if (orgName && repoName){
+          const releases = await valist.getReleasesFromRepo(orgName.toString(), repoName.toString());
+          if (releases) {
+              const latestTag = await valist.getLatestTagFromRepo(orgName, repoName);
+              let versions: any = {};
 
-        for (let i = 0; i < releases.length; i++) {
-            versions[releases[i].tag] = {
-                name: repoName,
-                version: releases[i].tag,
-                repository: "",
-                contributors: "",
-                bugs: "",
-                homepage: "",
-                dependencies: {},
-                dist: {
-                    tarball: `https://ipfs.fleek.co/ipfs/${releases[i].releaseCID}`
-                }
-            };
+              for (let i = 0; i < releases.length; i++) {
+                  versions[releases[i].tag] = {
+                      name: repoName,
+                      version: releases[i].tag,
+                      repository: "",
+                      contributors: "",
+                      bugs: "",
+                      homepage: "",
+                      dependencies: {},
+                      dist: {
+                          tarball: `https://ipfs.fleek.co/ipfs/${releases[i].releaseCID}`
+                      }
+                  };
+              }
+
+              return res.status(200).json({
+                  _id: "",
+                  name: "",
+                  "dist-tags": {
+                      latest: latestTag
+                  },
+                  versions
+              });
         }
-
-        return res.status(200).json({
-            _id: "",
-            name: "",
-            "dist-tags": {
-                latest: latestTag
-            },
-            versions
-        });
-
-    } else {
-        return res.status(500).json({statusCode: 500, message: "No Web3 Provider!"});
+      }
+        console.log("Package not Registered on Valist");
+        console.log(`Fetching Package ${releaseName} from https://registry.npmjs.org`);
+        return res.redirect(`https://registry.npmjs.org/${releaseName}`);
     }
+    return res.status(500).json({statusCode: 500, message: "No Web3 Provider!"});
 }
