5517d0ecda6b40285edcb6184247fcb025020a60
valist-dev <zach@valist.io>
2021-03-09 18:27:05 -0500

    handle npm fallback when not in valist

--- relay/pages/api/npm/[releaseName]/index.tsx
+++ relay/pages/api/npm/[releaseName]/index.tsx
@@ -17,40 +17,44 @@ export default async function getReleasesFromRepo(req: NextApiRequest, res: Next
 
         const [orgName, repoName] = decodeURIComponent(releaseName.toString().replace("@", "")).split("/");
 
-        if (orgName && repoName){
-          const releases = await valist.getReleasesFromRepo(orgName.toString(), repoName.toString());
-          if (releases) {
-              const latestTag = await valist.getLatestTagFromRepo(orgName, repoName);
-              let versions: any = {};
+        if (orgName && repoName) {
+          try {
+            const releases = await valist.getReleasesFromRepo(orgName.toString(), repoName.toString());
+            if (releases) {
+                const latestTag = await valist.getLatestTagFromRepo(orgName, repoName);
+                let versions: any = {};
 
-              for (let i = 0; i < releases.length; i++) {
-                  versions[releases[i].tag] = {
-                      name: repoName,
-                      version: releases[i].tag,
-                      repository: "",
-                      contributors: "",
-                      bugs: "",
-                      homepage: "",
-                      dependencies: {},
-                      dist: {
-                          tarball: `https://ipfs.fleek.co/ipfs/${releases[i].releaseCID}`
-                      }
-                  };
-              }
+                for (let i = 0; i < releases.length; i++) {
+                    versions[releases[i].tag] = {
+                        name: repoName,
+                        version: releases[i].tag,
+                        repository: "",
+                        contributors: "",
+                        bugs: "",
+                        homepage: "",
+                        dependencies: {},
+                        dist: {
+                            tarball: `https://ipfs.fleek.co/ipfs/${releases[i].releaseCID}`
+                        }
+                    };
+                }
 
-              return res.status(200).json({
-                  _id: "",
-                  name: "",
-                  "dist-tags": {
-                      latest: latestTag
-                  },
-                  versions
-              });
-        }
+                return res.status(200).json({
+                    _id: "",
+                    name: "",
+                    "dist-tags": {
+                        latest: latestTag
+                    },
+                    versions
+                });
+            }
+          } catch (e) {
+            console.log("Could not find package in Valist");
+          }
       }
-        console.log("Package not Registered on Valist");
-        console.log(`Fetching Package ${releaseName} from https://registry.npmjs.org`);
-        return res.redirect(`https://registry.npmjs.org/${releaseName}`);
+      console.log("Package not Registered on Valist");
+      console.log(`Fetching Package ${releaseName} from https://registry.npmjs.org`);
+      return res.redirect(`https://registry.npmjs.org/${releaseName}`);
     }
     return res.status(404).json({statusCode: 404, message: "Package Not Found"});
 }
