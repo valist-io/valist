e3f9eeb6d192e2c1d2de8202739b9c19ff473b0c
Alec M. Wantoch <alec@wantoch.com>
2021-04-07 22:18:09 -0400

    metatransactions in cli

--- cli/src/lib/crypto.ts
+++ cli/src/lib/crypto.ts
@@ -1,12 +1,14 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-const mnemonic = process.env.MNEMONIC;
-const key = process.env.KEY;
+// const mnemonic = process.env.MNEMONIC;
+const key = process.env.VALIST_SIGNING_KEY;
+
+if (/* !mnemonic && */ !key) throw new Error('No key found!');
 
 export const web3Provider = new HDWalletProvider({
-  mnemonic: { phrase: mnemonic },
+//  mnemonic: { phrase: mnemonic },
   privateKeys: [key],
-  providerOrUrl: 'https://rpc-mumbai.matic.today',
+  providerOrUrl: process.env.WEB3_PROVIDER || 'https://rpc-mumbai.matic.today',
 });
 
 export const getWeb3Provider = () => web3Provider;
