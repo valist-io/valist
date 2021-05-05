b0849aea9775250b6c40d14eeb7813d0a5cf4584
jiyuu-jin <zach@valist.io>
2021-05-04 21:25:42 -0400

    Add client-side getRelease with valist gateway.

--- relay/components/Releases/ReleaseList.tsx
+++ relay/components/Releases/ReleaseList.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react';
-import Link from 'next/link';
 import ValistContext from '../Valist/ValistContext';
 import NavTree from '../Navigation/NavTree';
 
@@ -7,6 +6,11 @@ export const ReleaseList = ({ orgName, repoName }: { orgName: string, repoName: 
   const valist = useContext(ValistContext);
 
   const [releases, setReleases] = useState(['Loading...']);
+
+  const getRelease = async (tag: string) => {
+    const { releaseCID } = await valist.getReleaseByTag(orgName, repoName, tag);
+    window.location.assign(`https://gateway.valist.io/ipfs/${releaseCID}`);
+  };
 
   useEffect(() => {
     (async () => {
@@ -30,29 +34,28 @@ export const ReleaseList = ({ orgName, repoName }: { orgName: string, repoName: 
             </div>
             <ul className="relative z-0 divide-y divide-gray-200 border-b border-gray-200">
             {[...releases].reverse().map((tag) => (
-                    <Link href={`/api/${orgName}/${repoName}/${tag}`} key={tag}>
-                        <li className="relative pl-4 pr-6 py-5 hover:bg-gray-50 sm:py-6 sm:pl-6 lg:pl-8 xl:pl-6">
-                            <div className="flex items-center justify-between space-x-4">
-                                <div className="min-w-0 space-y-3">
-                                    <div className="flex items-center space-x-3">
-                                        <span aria-label="Running" className="h-4 w-4 bg-green-100 rounded-full
-                                        flex items-center justify-center">
-                                            <span className="h-2 w-2 bg-green-400 rounded-full"></span>
-                                        </span>
+              <li key={tag} onClick={() => getRelease(tag)}
+              className="relative pl-4 pr-6 py-5 hover:bg-gray-50 sm:py-6 sm:pl-6 lg:pl-8 xl:pl-6">
+                  <div className="flex items-center justify-between space-x-4">
+                      <div className="min-w-0 space-y-3">
+                          <div className="flex items-center space-x-3">
+                              <span aria-label="Running" className="h-4 w-4 bg-green-100 rounded-full
+                              flex items-center justify-center">
+                                  <span className="h-2 w-2 bg-green-400 rounded-full"></span>
+                              </span>
 
-                                        <span className="block">
-                                            <h2 className="text-sm font-medium leading-5">
-                                                <a href="#">
-                                                    <span className="absolute inset-0"></span>
-                                                    {tag}
-                                                </a>
-                                            </h2>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </Link>
+                              <span className="block">
+                                  <h2 className="text-sm font-medium leading-5">
+                                      <a href="#">
+                                          <span className="absolute inset-0"></span>
+                                          {tag}
+                                      </a>
+                                  </h2>
+                              </span>
+                          </div>
+                      </div>
+                  </div>
+              </li>
             ))}
             </ul>
         </div>
