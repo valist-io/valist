bb00ffd7f8e0020b27d76b07631facaba91f4395
jiyuu-jin <zach@valist.io>
2021-07-07 17:16:33 -0400

    Fix biconomy meta transactions & replace ethers with web3js
    Co-authored-by: Alec M. Wantoch <awantoch@users.noreply.github.com>

--- lib/src/helpers/biconomy.ts
+++ lib/src/helpers/biconomy.ts
@@ -1,60 +1,58 @@
-// @ts-nocheck
-import { ethers } from 'ethers';
-const abi = require('ethereumjs-abi');
-let helperAttributes = {};
-let supportedNetworks = [42, 4, 5, 80001]; //add more
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+const helperAttributes: any = {};
 helperAttributes.ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 helperAttributes.baseURL = 'https://api.biconomy.io';
-// any other constants needed goes in helperAttributes
 
+// eslint-disable-next-line
 helperAttributes.biconomyForwarderAbi = [{'inputs':[{'internalType':'address','name':'_owner','type':'address'}],'stateMutability':'nonpayable','type':'constructor'},{'anonymous':false,'inputs':[{'indexed':true,'internalType':'bytes32','name':'domainSeparator','type':'bytes32'},{'indexed':false,'internalType':'bytes','name':'domainValue','type':'bytes'}],'name':'DomainRegistered','type':'event'},{'anonymous':false,'inputs':[{'indexed':true,'internalType':'address','name':'previousOwner','type':'address'},{'indexed':true,'internalType':'address','name':'newOwner','type':'address'}],'name':'OwnershipTransferred','type':'event'},{'inputs':[],'name':'EIP712_DOMAIN_TYPE','outputs':[{'internalType':'string','name':'','type':'string'}],'stateMutability':'view','type':'function'},{'inputs':[],'name':'REQUEST_TYPEHASH','outputs':[{'internalType':'bytes32','name':'','type':'bytes32'}],'stateMutability':'view','type':'function'},{'inputs':[{'internalType':'bytes32','name':'','type':'bytes32'}],'name':'domains','outputs':[{'internalType':'bool','name':'','type':'bool'}],'stateMutability':'view','type':'function'},{'inputs':[{'components':[{'internalType':'address','name':'from','type':'address'},{'internalType':'address','name':'to','type':'address'},{'internalType':'address','name':'token','type':'address'},{'internalType':'uint256','name':'txGas','type':'uint256'},{'internalType':'uint256','name':'tokenGasPrice','type':'uint256'},{'internalType':'uint256','name':'batchId','type':'uint256'},{'internalType':'uint256','name':'batchNonce','type':'uint256'},{'internalType':'uint256','name':'deadline','type':'uint256'},{'internalType':'bytes','name':'data','type':'bytes'}],'internalType':'structERC20ForwardRequestTypes.ERC20ForwardRequest','name':'req','type':'tuple'},{'internalType':'bytes32','name':'domainSeparator','type':'bytes32'},{'internalType':'bytes','name':'sig','type':'bytes'}],'name':'executeEIP712','outputs':[{'internalType':'bool','name':'success','type':'bool'},{'internalType':'bytes','name':'ret','type':'bytes'}],'stateMutability':'nonpayable','type':'function'},{'inputs':[{'components':[{'internalType':'address','name':'from','type':'address'},{'internalType':'address','name':'to','type':'address'},{'internalType':'address','name':'token','type':'address'},{'internalType':'uint256','name':'txGas','type':'uint256'},{'internalType':'uint256','name':'tokenGasPrice','type':'uint256'},{'internalType':'uint256','name':'batchId','type':'uint256'},{'internalType':'uint256','name':'batchNonce','type':'uint256'},{'internalType':'uint256','name':'deadline','type':'uint256'},{'internalType':'bytes','name':'data','type':'bytes'}],'internalType':'structERC20ForwardRequestTypes.ERC20ForwardRequest','name':'req','type':'tuple'},{'internalType':'bytes','name':'sig','type':'bytes'}],'name':'executePersonalSign','outputs':[{'internalType':'bool','name':'success','type':'bool'},{'internalType':'bytes','name':'ret','type':'bytes'}],'stateMutability':'nonpayable','type':'function'},{'inputs':[{'internalType':'address','name':'from','type':'address'},{'internalType':'uint256','name':'batchId','type':'uint256'}],'name':'getNonce','outputs':[{'internalType':'uint256','name':'','type':'uint256'}],'stateMutability':'view','type':'function'},{'inputs':[],'name':'isOwner','outputs':[{'internalType':'bool','name':'','type':'bool'}],'stateMutability':'view','type':'function'},{'inputs':[],'name':'owner','outputs':[{'internalType':'address','name':'','type':'address'}],'stateMutability':'view','type':'function'},{'inputs':[{'internalType':'string','name':'name','type':'string'},{'internalType':'string','name':'version','type':'string'}],'name':'registerDomainSeparator','outputs':[],'stateMutability':'nonpayable','type':'function'},{'inputs':[],'name':'renounceOwnership','outputs':[],'stateMutability':'nonpayable','type':'function'},{'inputs':[{'internalType':'address','name':'newOwner','type':'address'}],'name':'transferOwnership','outputs':[],'stateMutability':'nonpayable','type':'function'},{'inputs':[{'components':[{'internalType':'address','name':'from','type':'address'},{'internalType':'address','name':'to','type':'address'},{'internalType':'address','name':'token','type':'address'},{'internalType':'uint256','name':'txGas','type':'uint256'},{'internalType':'uint256','name':'tokenGasPrice','type':'uint256'},{'internalType':'uint256','name':'batchId','type':'uint256'},{'internalType':'uint256','name':'batchNonce','type':'uint256'},{'internalType':'uint256','name':'deadline','type':'uint256'},{'internalType':'bytes','name':'data','type':'bytes'}],'internalType':'structERC20ForwardRequestTypes.ERC20ForwardRequest','name':'req','type':'tuple'},{'internalType':'bytes32','name':'domainSeparator','type':'bytes32'},{'internalType':'bytes','name':'sig','type':'bytes'}],'name':'verifyEIP712','outputs':[],'stateMutability':'view','type':'function'},{'inputs':[{'components':[{'internalType':'address','name':'from','type':'address'},{'internalType':'address','name':'to','type':'address'},{'internalType':'address','name':'token','type':'address'},{'internalType':'uint256','name':'txGas','type':'uint256'},{'internalType':'uint256','name':'tokenGasPrice','type':'uint256'},{'internalType':'uint256','name':'batchId','type':'uint256'},{'internalType':'uint256','name':'batchNonce','type':'uint256'},{'internalType':'uint256','name':'deadline','type':'uint256'},{'internalType':'bytes','name':'data','type':'bytes'}],'internalType':'structERC20ForwardRequestTypes.ERC20ForwardRequest','name':'req','type':'tuple'},{'internalType':'bytes','name':'sig','type':'bytes'}],'name':'verifyPersonalSign','outputs':[],'stateMutability':'view','type':'function'}];
 
 helperAttributes.biconomyForwarderDomainData = {
-    name : 'Biconomy Forwarder',
-    version : '1',
-  };
+  name: 'Biconomy Forwarder',
+  version: '1',
+};
 
 helperAttributes.domainType = [
-    { name: 'name', type: 'string' },
-    { name: 'version', type: 'string' },
-    { name: 'verifyingContract', type: 'address' },
-    { name: 'salt', type: 'bytes32' },
-  ];
+  { name: 'name', type: 'string' },
+  { name: 'version', type: 'string' },
+  { name: 'verifyingContract', type: 'address' },
+  { name: 'salt', type: 'bytes32' },
+];
 
 helperAttributes.forwardRequestType = [
-    {name:'from',type:'address'},
-    {name:'to',type:'address'},
-    {name:'token',type:'address'},
-    {name:'txGas',type:'uint256'},
-    {name:'tokenGasPrice',type:'uint256'},
-    {name:'batchId',type:'uint256'},
-    {name:'batchNonce',type:'uint256'},
-    {name:'deadline',type:'uint256'},
-    {name:'data',type:'bytes'}
-  ];
+  { name: 'from', type: 'address' },
+  { name: 'to', type: 'address' },
+  { name: 'token', type: 'address' },
+  { name: 'txGas', type: 'uint256' },
+  { name: 'tokenGasPrice', type: 'uint256' },
+  { name: 'batchId', type: 'uint256' },
+  { name: 'batchNonce', type: 'uint256' },
+  { name: 'deadline', type: 'uint256' },
+  { name: 'data', type: 'bytes' },
+];
 
 // pass the networkId to get contract addresses
-const getContractAddresses = async (networkId) => {
-    let contractAddresses = {};
-    const apiInfo = `${
-        helperAttributes.baseURL
-    }/api/v2/meta-tx/systemInfo?networkId=${networkId}`;
-    const response = await fetch(apiInfo);
-    const systemInfo = await response.json();
-    contractAddresses.biconomyForwarderAddress = systemInfo.biconomyForwarderAddress;
-    return contractAddresses;
+const getContractAddresses = async (networkId: number) => {
+  const addressMap: Record<number, string> = {
+    80001: '0x9399BB24DBB5C4b782C70c2969F58716Ebbd6a3b',
+    137: '0x86C80a8aa58e0A4fa09A69624c31Ab2a6CAD56b8',
+    1: '0x84a0856b038eaAd1cC7E297cF34A7e72685A8693',
   };
+  const contractAddresses: any = {};
+  contractAddresses.biconomyForwarderAddress = addressMap[networkId || 80001];
+  return contractAddresses;
+};
 
 /**
  * Returns ABI and contract address based on network Id
  * You can build biconomy forwarder contract object using above values and calculate the nonce
  * @param {*} networkId
  */
-const getBiconomyForwarderConfig = async (networkId) => {
-        //get trusted forwarder contract address from network id
-        const contractAddresses = await getContractAddresses(networkId);
-        const forwarderAddress = contractAddresses.biconomyForwarderAddress;
-        return {abi: helperAttributes.biconomyForwarderAbi, address: forwarderAddress};
+const getBiconomyForwarderConfig = async (networkId: number) => {
+  // get trusted forwarder contract address from network id
+  const contractAddresses = await getContractAddresses(networkId);
+  const forwarderAddress = contractAddresses.biconomyForwarderAddress;
+  return { abi: helperAttributes.biconomyForwarderAbi, address: forwarderAddress };
 };
 
 /**
@@ -67,19 +65,23 @@ const getBiconomyForwarderConfig = async (networkId) => {
  * @param {*}  data - functionSignature of target method
  * @param {*}  deadline - optional deadline for this forward request
  */
-const buildForwardTxRequest = async ({account, to, gasLimitNum, batchId, batchNonce, data, deadline}) => {
-    const req = {
-        from: account,
-        to: to,
-        token: helperAttributes.ZERO_ADDRESS,
-        txGas: gasLimitNum,
-        tokenGasPrice: '0',
-        batchId: parseInt(batchId),
-        batchNonce: parseInt(batchNonce),
-        deadline: deadline || Math.floor(Date.now() / 1000 + 3600),
-        data: data
-    };
-    return req;
+const buildForwardTxRequest = async ({
+  account, to, gasLimitNum, batchId, batchNonce, data, deadline,
+}: any) => {
+  const req = {
+    from: account,
+    to,
+    token: helperAttributes.ZERO_ADDRESS,
+    txGas: gasLimitNum,
+    tokenGasPrice: '0',
+    // eslint-disable-next-line
+    batchId: parseInt(batchId),
+    // eslint-disable-next-line
+    batchNonce: parseInt(batchNonce),
+    deadline: deadline || Math.floor(Date.now() / 1000 + 3600),
+    data,
+  };
+  return req;
 };
 
 /**
@@ -88,87 +90,58 @@ const buildForwardTxRequest = async ({account, to, gasLimitNum, batchId, batchNo
  * @param {*} request - forward request object
  * @param {*} networkId
  */
-const getDataToSignForEIP712 = async (request,networkId) => {
-    const contractAddresses = await getContractAddresses(networkId);
-    const forwarderAddress = contractAddresses.biconomyForwarderAddress;
-    let domainData = helperAttributes.biconomyForwarderDomainData;
-    domainData.salt = ethers.utils.hexZeroPad((ethers.BigNumber.from(networkId)).toHexString(), 32);
-    domainData.verifyingContract = forwarderAddress;
+const getDataToSignForEIP712 = async (web3: any, request: any, networkId: number) => {
+  const contractAddresses = await getContractAddresses(networkId);
+  const forwarderAddress = contractAddresses.biconomyForwarderAddress;
+  const domainData = helperAttributes.biconomyForwarderDomainData;
+  domainData.salt = web3.utils.padLeft(web3.utils.toHex(web3.utils.toBN(networkId)), 64);
+  domainData.verifyingContract = forwarderAddress;
 
-    const dataToSign = JSON.stringify({
-        types: {
-            EIP712Domain: helperAttributes.domainType,
-            ERC20ForwardRequest: helperAttributes.forwardRequestType
-        },
-        domain: domainData,
-        primaryType: 'ERC20ForwardRequest',
-        message: request
-    });
-    return dataToSign;
-}
-
-/**
- * pass your forward request
- * use this method to build message to be signed by end user in personal signature format
- * @param {*} networkId
- */
-const getDataToSignForPersonalSign = (request) => {
-    const hashToSign = abi.soliditySHA3([
-        'address',
-        'address',
-        'address',
-        'uint256',
-        'uint256',
-        'uint256',
-        'uint256',
-        'uint256',
-        'bytes32',
-    ], [
-        request.from,
-        request.to,
-        request.token,
-        request.txGas,
-        request.tokenGasPrice,
-        request.batchId,
-        request.batchNonce,
-        request.deadline,
-        ethers.utils.keccak256(request.data),
-    ]);
-    return hashToSign;
-}
+  const dataToSign = JSON.stringify({
+    types: {
+      EIP712Domain: helperAttributes.domainType,
+      ERC20ForwardRequest: helperAttributes.forwardRequestType,
+    },
+    domain: domainData,
+    primaryType: 'ERC20ForwardRequest',
+    message: request,
+  });
+  return dataToSign;
+};
 
 /**
  * get the domain seperator that needs to be passed while using EIP712 signature type
  * @param {*} networkId
  */
-const getDomainSeperator = async (networkId) => {
-    const contractAddresses = await getContractAddresses(networkId);
-    const forwarderAddress = contractAddresses.biconomyForwarderAddress;
-    let domainData = helperAttributes.biconomyForwarderDomainData;
-    domainData.salt = ethers.utils.hexZeroPad((ethers.BigNumber.from(networkId)).toHexString(), 32);
-    domainData.verifyingContract = forwarderAddress;
+// eslint-disable-next-line
+const getDomainSeperator = async (web3: any, networkId: number) => {
+  const contractAddresses = await getContractAddresses(networkId);
+  const forwarderAddress = contractAddresses.biconomyForwarderAddress;
+  const domainData = helperAttributes.biconomyForwarderDomainData;
+  domainData.salt = web3.utils.padLeft(web3.utils.toHex(web3.utils.toBN(networkId)), 64);
+  domainData.verifyingContract = forwarderAddress;
 
-    const domainSeparator = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode([
-        'bytes32',
-        'bytes32',
-        'bytes32',
-        'address',
-        'bytes32'
+  const domainSeparator = web3.utils.keccak256(
+    web3.eth.abi.encodeParameters([
+      'bytes32',
+      'bytes32',
+      'bytes32',
+      'address',
+      'bytes32',
     ], [
-        ethers.utils.id('EIP712Domain(string name,string version,address verifyingContract,bytes32 salt)'),
-        ethers.utils.id(domainData.name),
-        ethers.utils.id(domainData.version),
-        domainData.verifyingContract,
-        domainData.salt,
-    ]));
-    return domainSeparator;
+      web3.utils.keccak256('EIP712Domain(string name,string version,address verifyingContract,bytes32 salt)'),
+      web3.utils.keccak256(domainData.name),
+      web3.utils.keccak256(domainData.version),
+      domainData.verifyingContract,
+      domainData.salt,
+    ]),
+  );
+  return domainSeparator;
 };
 
 export {
-    helperAttributes,
-    getDomainSeperator,
-    getDataToSignForPersonalSign,
-    getDataToSignForEIP712,
-    buildForwardTxRequest,
-    getBiconomyForwarderConfig
+  getDomainSeperator,
+  getDataToSignForEIP712,
+  buildForwardTxRequest,
+  getBiconomyForwarderConfig,
 };
