240bae2d0ba5409002a88cc71b1701674333bd74
Zachary Pelkey <zpelkey@gmail.com>
2021-07-08 00:05:14 -0400

    Eslint fix/ add airbnb style guide

---
+++ cli/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+  ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
+    // 'oclif',
+    // 'oclif-typescript',
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', { 'code': 120 }],
+  }
+};
