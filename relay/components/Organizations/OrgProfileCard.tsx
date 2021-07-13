a02613e886d375ced30dc10601ca5d60e454f5c5
Alec M. Wantoch <alec@wantoch.com>
2021-07-13 16:50:11 -0400

    adjust padding/margins on org & project profile cards

--- relay/components/Organizations/OrgProfileCard.tsx
+++ relay/components/Organizations/OrgProfileCard.tsx
@@ -25,11 +25,11 @@ const OrgProfileCard = ({ orgName }: any) => {
         <h2 className="sr-only" id="profile-overview-title">Profile Overview</h2>
         <div className="bg-white p-6">
           <div className="sm:flex sm:items-center sm:justify-between">
-            <div className="sm:flex sm:space-x-5">
-              <div className="flex-shrink-0">
+            <div className="sm:flex sm:items-center sm:space-x-5">
+              <div>
                 <AddressIdenticon address={orgMeta.name} height={20}/>
               </div>
-              <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
+              <div>
                 <p className="text-sm font-medium text-gray-600"></p>
                 <p className="text-xl font-bold text-gray-900 sm:text-2xl">{orgMeta.name}</p>
                 <p className="text-sm font-medium text-gray-600">{orgMeta.description}</p>
