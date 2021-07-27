73aa91db3889c5bd74cc5327e812d94065bf9b51
Zachary Pelkey <zpelkey@gmail.com>
2021-07-27 17:59:31 -0400

    Remove unecessary test & update API usage in tests

--- internal/build/config_test.go
+++ internal/build/config_test.go
@@ -1,40 +1,20 @@
 package build
 
 import (
-	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateValistConfig(t *testing.T) {
+	tmp, err := os.MkdirTemp("", "test")
+	require.NoError(t, err, "Failed to create tmp dir")
+	defer os.RemoveAll(tmp)
 
-	CreateValistConfig(
-		"go",
-		"test",
-		"binary",
-		"0.0.2",
-		"README.md",
-		"make all",
-		"go mod tidy",
-		"dist",
-		map[string]string{
-			"linux/amd64":  "bin/lin000x/hello-world",
-			"darwin/amd64": "bin/macz/hello-world",
-		},
-	)
-
-	assert.FileExists(t, "valist.yml", "Valist file has been created")
-}
-
-func TestParseValistConfig(t *testing.T) {
-
-	config, err := ParseValistConfig()
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	testConfig := ValistConfig{
+	var config = Config{
 		Type:    "go",
 		Org:     "test",
 		Repo:    "binary",
@@ -49,5 +29,18 @@ func TestParseValistConfig(t *testing.T) {
 		},
 	}
 
-	assert.Equal(t, config, testConfig, "Parsed config matches expected result")
+	cfgPath := filepath.Join(tmp, "valist.yml")
+	err = config.Save(cfgPath)
+	require.NoError(t, err, "Failed to create tmp dir")
+	assert.FileExists(t, cfgPath, "Valist file has been created")
+
+	var other Config
+	err = other.Load(cfgPath)
+	require.NoError(t, err, "Failed to load config")
+	assert.Equal(t, config, other)
+
+	// var other2 Config
+	// err = other2.Load("testdata/2.valist.yml")
+	// require.NoError(t, err, "Failed to load config")
+	// assert.Equal(t, config, other2)
 }
