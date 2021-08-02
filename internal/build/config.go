ae75fcdb944c777a13dad370bd82e351ea991518
Zachary Pelkey <zpelkey@gmail.com>
2021-08-02 15:28:15 -0400

    Moved interactive logic  to sperate function

--- internal/build/config.go
+++ internal/build/config.go
@@ -1,8 +1,11 @@
 package build
 
 import (
+	"errors"
+	"fmt"
 	"os"
 
+	"github.com/manifoldco/promptui"
 	"gopkg.in/yaml.v3"
 )
 
@@ -70,6 +73,188 @@ func (c *Config) Load(path string) error {
 	}
 
 	return yaml.Unmarshal(yamlFile, c)
+}
+
+func validateLength(value string) error {
+	if len(value) > 0 {
+		return nil
+	}
+	return errors.New("Length must be greater than 0")
+}
+
+func validateYesNo(value string) error {
+	switch value {
+	case
+		"y",
+		"Y",
+		"n",
+		"N":
+		return nil
+	}
+	return errors.New("Must be y or n")
+}
+
+func GenerateFileInteractive(projectType string) error {
+	var out string
+
+	orgPrompt := promptui.Prompt{
+		Label:    "Organization name or username",
+		Validate: validateLength,
+	}
+	org, err := orgPrompt.Run()
+	if err != nil {
+		return err
+	}
+
+	repoPrompt := promptui.Prompt{
+		Label:    "Repository name",
+		Validate: validateLength,
+	}
+	repo, err := repoPrompt.Run()
+	if err != nil {
+		return err
+	}
+
+	tagPrompt := promptui.Prompt{
+		Label:   "Release tag",
+		Default: "0.0.1",
+	}
+	tag, err := tagPrompt.Run()
+	if err != nil {
+		return err
+	}
+
+	metaPrompt := promptui.Prompt{
+		Label: "Path to meta file(README.md)",
+	}
+	meta, err := metaPrompt.Run()
+	if err != nil {
+		return err
+	}
+
+	defaultInstall := DefaultInstalls[projectType]
+	installPrompt := promptui.Prompt{
+		Label:   "Command used to install dependencies",
+		Default: defaultInstall,
+	}
+	install, err := installPrompt.Run()
+	if err != nil {
+		return err
+	}
+
+	defaultBuild := DefaultBuilds[projectType]
+	buildPrompt := promptui.Prompt{
+		Label:   "Command used to build your project",
+		Default: defaultBuild,
+	}
+	buildCommand, err := buildPrompt.Run()
+	if err != nil {
+		return err
+	}
+
+	defaultImage := DefaultImages[projectType]
+	imagePrompt := promptui.Prompt{
+		Label: fmt.Sprintf("Docker image (if not set, will default to %s)", defaultImage),
+	}
+	image, err := imagePrompt.Run()
+	if err != nil {
+		return err
+	}
+
+	// If the project type is not node prompt for out path
+	if projectType != "node" {
+		outPrompt := promptui.Prompt{
+			Label: "Build output file/directory",
+		}
+		out, err = outPrompt.Run()
+		if err != nil {
+			return err
+		}
+	}
+
+	// Create valist config with empty artifacts mapping
+	var cfg = Config{
+		Type:      projectType,
+		Org:       org,
+		Repo:      repo,
+		Tag:       tag,
+		Meta:      meta,
+		Image:     image,
+		Build:     buildCommand,
+		Install:   install,
+		Out:       out,
+		Artifacts: map[string]string{},
+	}
+
+	artifactsPrompt := promptui.Prompt{
+		Label:     "Are you building for multiple architecures?",
+		IsConfirm: true,
+		Validate:  validateYesNo,
+	}
+	// If there is artifacts set isArtifacts to y
+	isArtifacts, err := artifactsPrompt.Run()
+	if err != nil {
+	}
+
+	// If there are artifacts then prompt for their os, arch, & name/path
+	for isArtifacts == "y" || isArtifacts == "Y" {
+		osPrompt := promptui.Prompt{
+			Label: "Artifact operating system (leave blank to quit)",
+		}
+
+		osName, err := osPrompt.Run()
+		if err != nil {
+			return err
+		}
+
+		if osName == "" {
+			break
+		}
+
+		archPrompt := promptui.Prompt{
+			Label: "Artifact architecture ",
+		}
+
+		arch, err := archPrompt.Run()
+		if err != nil {
+			return err
+		}
+
+		srcPrompt := promptui.Prompt{
+			Label: "Artifact source",
+		}
+
+		src, err := srcPrompt.Run()
+		if err != nil {
+			return err
+		}
+
+		// Set artifact key to os/arch and value to src
+		cfg.Artifacts[fmt.Sprintf("%s/%s", osName, arch)] = src
+	}
+
+	return cfg.Save("valist.yml")
+}
+
+func GenerateValistFile(projectType string) error {
+
+	var cfg = Config{
+		Type:    projectType,
+		Org:     " ",
+		Repo:    " ",
+		Tag:     " ",
+		Meta:    " ",
+		Image:   " ",
+		Build:   " ",
+		Install: " ",
+		Out:     " ",
+	}
+
+	if projectType == "node" {
+		return cfg.Save("valist.yml")
+	}
+
+	return cfg.Save("valist.yml")
 }
 
 // https://pkg.go.dev/github.com/go-playground/validator/v10
--- internal/command/init.go
+++ internal/command/init.go
@@ -1,40 +1,31 @@
 package command
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/manifoldco/promptui"
 	"github.com/urfave/cli/v2"
 	"github.com/valist-io/registry/internal/build"
 )
 
-func validateLength(value string) error {
-	if len(value) > 0 {
-		return nil
-	}
-	return errors.New("Length must be greater than 0")
-}
-
-func validateYesNo(value string) error {
-	switch value {
-	case
-		"y",
-		"Y",
-		"n",
-		"N":
-		return nil
-	}
-	return errors.New("Must be y or n")
-}
-
 func NewInitCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "init",
 		Usage: "Generate a new Valist project",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "type",
+				Usage: "Set project type",
+			},
+			&cli.BoolFlag{
+				Name:  "interactive",
+				Value: true,
+				Usage: "Enable interactive mode",
+			},
+		},
 		Action: func(c *cli.Context) error {
-			var out string
+			// Get interactive flag value
+			isInteractive := c.String("interactive")
 
+			// If project type is not set ask for projectType
 			projectPrompt := promptui.Select{
 				Label: "Repository type",
 				Items: []string{
@@ -46,143 +37,13 @@ func NewInitCommand() *cli.Command {
 				return err
 			}
 
-			orgPrompt := promptui.Prompt{
-				Label:    "Organization name or username",
-				Validate: validateLength,
-			}
-			org, err := orgPrompt.Run()
-			if err != nil {
-				return err
+			if isInteractive == "true" {
+				build.GenerateFileInteractive(projectType)
+			} else {
+				build.GenerateValistFile(projectType)
 			}
 
-			repoPrompt := promptui.Prompt{
-				Label:    "Repository name",
-				Validate: validateLength,
-			}
-			repo, err := repoPrompt.Run()
-			if err != nil {
-				return err
-			}
-
-			tagPrompt := promptui.Prompt{
-				Label:   "Release tag",
-				Default: "0.0.1",
-			}
-			tag, err := tagPrompt.Run()
-			if err != nil {
-				return err
-			}
-
-			metaPrompt := promptui.Prompt{
-				Label: "Path to meta file(README.md)",
-			}
-			meta, err := metaPrompt.Run()
-			if err != nil {
-				return err
-			}
-
-			defaultInstall := build.DefaultInstalls[projectType]
-			installPrompt := promptui.Prompt{
-				Label:   "Command used to install dependencies",
-				Default: defaultInstall,
-			}
-			install, err := installPrompt.Run()
-			if err != nil {
-				return err
-			}
-
-			defaultBuild := build.DefaultBuilds[projectType]
-			buildPrompt := promptui.Prompt{
-				Label:   "Command used to build your project",
-				Default: defaultBuild,
-			}
-			buildCommand, err := buildPrompt.Run()
-			if err != nil {
-				return err
-			}
-
-			defaultImage := build.DefaultImages[projectType]
-			imagePrompt := promptui.Prompt{
-				Label: fmt.Sprintf("Docker image (if not set, will default to %s)", defaultImage),
-			}
-			image, err := imagePrompt.Run()
-			if err != nil {
-				return err
-			}
-
-			// If the project type is not node prompt for out path
-			if projectType != "node" {
-				outPrompt := promptui.Prompt{
-					Label: "Build output file/directory",
-				}
-				out, err = outPrompt.Run()
-				if err != nil {
-					return err
-				}
-			}
-
-			// Create valist config with empty artifacts mapping
-			var cfg = build.Config{
-				Type:      projectType,
-				Org:       org,
-				Repo:      repo,
-				Tag:       tag,
-				Meta:      meta,
-				Image:     image,
-				Build:     buildCommand,
-				Install:   install,
-				Out:       out,
-				Artifacts: map[string]string{},
-			}
-
-			artifactsPrompt := promptui.Prompt{
-				Label:     "Are you building for multiple architecures?",
-				IsConfirm: true,
-				Validate:  validateYesNo,
-			}
-			// If there is artifacts set isArtifacts to y
-			isArtifacts, err := artifactsPrompt.Run()
-			if err != nil {
-			}
-
-			// If there are artifacts then prompt for their os, arch, & name/path
-			for isArtifacts == "y" || isArtifacts == "Y" {
-				osPrompt := promptui.Prompt{
-					Label: "Artifact operating system (leave blank to quit)",
-				}
-
-				osName, err := osPrompt.Run()
-				if err != nil {
-					return err
-				}
-
-				if osName == "" {
-					break
-				}
-
-				archPrompt := promptui.Prompt{
-					Label: "Artifact architecture ",
-				}
-
-				arch, err := archPrompt.Run()
-				if err != nil {
-					return err
-				}
-
-				srcPrompt := promptui.Prompt{
-					Label: "Artifact source",
-				}
-
-				src, err := srcPrompt.Run()
-				if err != nil {
-					return err
-				}
-
-				// Set artifact key to os/arch and value to src
-				cfg.Artifacts[fmt.Sprintf("%s/%s", osName, arch)] = src
-			}
-
-			return cfg.Save("valist.yml")
+			return nil
 		},
 	}
 }
