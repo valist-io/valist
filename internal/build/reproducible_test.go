af5ac4b7e65e7599c134d8c71edbfd357613dee1
Zachary Pelkey <zpelkey@gmail.com>
2021-08-05 14:40:04 -0400

    Added tmp directory to tests

--- internal/build/reproducible_test.go
+++ internal/build/reproducible_test.go
@@ -13,11 +13,10 @@ func TestCreateBuild(t *testing.T) {
 	tmp, err := os.MkdirTemp("", "test")
 	require.NoError(t, err, "Failed to create tmp dir")
 
-	ymlFilePath := filepath.Join(tmp, "valist.yml")
 	dockerFilePath := filepath.Join(tmp, "Dockerfile")
 
 	var dockerConfig = DockerConfig{
-		Path:         ymlFilePath,
+		Path:         dockerFilePath,
 		BaseImage:    "golang:buster",
 		Source:       "./",
 		BuildCommand: "go build -o ./dist/main testdata/main.go",
@@ -25,7 +24,7 @@ func TestCreateBuild(t *testing.T) {
 
 	err = GenerateDockerfile(dockerConfig)
 	assert.NoError(t, err, "Generate Dockerfile returns with no errors")
-	assert.FileExists(t, ymlFilePath, "Dockerfile has been created")
+	assert.FileExists(t, dockerFilePath, "Dockerfile has been created")
 
 	err = Create("valist-build", dockerFilePath)
 	assert.NoError(t, err, "Create build returns with no errors")
