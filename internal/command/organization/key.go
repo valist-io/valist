3b64114ce02c738c5ef01f68cd9445a8668d7998
Alec M. Wantoch <alec@wantoch.com>
2021-08-27 13:14:30 -0400

    VoteOrganizationAdmin CLI impl

---
+++ internal/command/organization/key.go
@@ -0,0 +1,104 @@
+package organization
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/urfave/cli/v2"
+
+	"github.com/valist-io/registry/internal/core"
+	"github.com/valist-io/registry/internal/core/client"
+	"github.com/valist-io/registry/internal/core/config"
+)
+
+func voteOrganizationAdmin(c *cli.Context, operation common.Hash) error {
+	if c.NArg() != 2 {
+		cli.ShowSubcommandHelpAndExit(c, 1)
+	}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	cfg, err := config.Load(home)
+	if err != nil {
+		return err
+	}
+
+	var account accounts.Account
+	if c.IsSet("account") {
+		account.Address = common.HexToAddress(c.String("account"))
+	} else {
+		account.Address = cfg.Accounts.Default
+	}
+
+	valist, err := core.NewClient(c.Context, cfg, account)
+	if err != nil {
+		return err
+	}
+	defer valist.Close()
+
+	// @TODO: Add extra validation here
+	orgName := c.Args().Get(0)
+
+	orgID, err := valist.GetOrganizationID(c.Context, orgName)
+	if err != nil {
+		return err
+	}
+
+	if !common.IsHexAddress(c.Args().Get(1)) {
+		return fmt.Errorf("Invalid address: %s", c.Args().Get(1))
+	}
+
+	address := common.HexToAddress(c.Args().Get(1))
+
+	vote, err := valist.VoteOrganizationAdmin(c.Context, orgID, operation, address)
+	if err != nil {
+		return err
+	}
+
+	if big.NewInt(1).Cmp(vote.Threshold) == -1 && vote.SigCount.Cmp(vote.Threshold) == -1 {
+		fmt.Printf("Voted to add key, %d/%d\n votes", vote.SigCount, vote.Threshold)
+	} else {
+		fmt.Printf("Approved adding key!")
+	}
+
+	return nil
+}
+
+func NewKeyCommand() *cli.Command {
+	return &cli.Command{
+		Name:  "key",
+		Usage: "Manage keys at an organization level",
+		Subcommands: []*cli.Command{
+			{
+				Name:  "add",
+				Usage: "Add a new key to an organization",
+				Action: func(c *cli.Context) error {
+					fmt.Println("Adding key to organization...")
+					return voteOrganizationAdmin(c, client.ADD_KEY)
+				},
+			},
+			{
+				Name:  "remove",
+				Usage: "Remove a key from an organization",
+				Action: func(c *cli.Context) error {
+					fmt.Println("Removing key to organization...")
+					return voteOrganizationAdmin(c, client.REVOKE_KEY)
+				},
+			},
+			{
+				Name:  "rotate",
+				Usage: "Replace a key on an organization",
+				Action: func(c *cli.Context) error {
+					fmt.Println("Rotating key on organization...")
+					return voteOrganizationAdmin(c, client.ROTATE_KEY)
+				},
+			},
+		},
+	}
+}
