3eaa88960e5abccab7ed7ae22a5fd4af3f17abb0
Alec M. Wantoch <alec@wantoch.com>
2021-09-02 11:59:10 -0400

    clean repo to prep for golang port

--- cli/src/utils/errors.ts
+++
@@ -1,6 +0,0 @@
-export class MissingKeyError extends Error {
-  constructor() {
-    super('🔎 Key not found! Use "valist account:new" to create a new key.');
-    this.name = 'MissingKeyError';
-  }
-}
