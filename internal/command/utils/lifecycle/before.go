4ecd8dd518efa1de770fd4096565936c44ac21b2
Alec M. Wantoch <alec@wantoch.com>
2021-09-15 17:35:06 -0400

    simplify client setup steps in CLI lifecycle

--- internal/command/utils/lifecycle/before.go
+++ internal/command/utils/lifecycle/before.go
@@ -2,10 +2,13 @@ package lifecycle
 
 import (
 	"context"
+	"crypto/rand"
+	"fmt"
 	"os"
 
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/urfave/cli/v2"
 
 	"github.com/valist-io/valist/internal/core"
@@ -14,26 +17,97 @@ import (
 	"github.com/valist-io/valist/internal/prompt"
 )
 
-func SetupClient(c *cli.Context) error {
+const passphraseKey = "passphrase"
+
+func loadConfig() (*config.Config, error) {
 	home, err := os.UserHomeDir()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if err := config.Initialize(home); err != nil {
-		return err
+		return nil, err
 	}
 
 	cfg := config.NewConfig(home)
+
 	if err := cfg.Load(); err != nil {
-		return err
+		return nil, err
 	}
 
+	return cfg, nil
+}
+
+func getAccount(c *cli.Context, cfg *config.Config) (accounts.Account, error) {
 	var account accounts.Account
 	if c.IsSet("account") {
 		account.Address = common.HexToAddress(c.String("account"))
 	} else {
 		account.Address = cfg.Accounts.Default
+	}
+
+	// use env variable VALIST_SIGNER as signing key when set
+	if os.Getenv("VALIST_SIGNER") != "" {
+		// generate cryptographically secure, ephemeral encryption key that will clear when program halts
+		randBytes := make([]byte, 32)
+		_, err := rand.Read(randBytes)
+		if err != nil {
+			return accounts.Account{}, err
+		}
+
+		passphrase := fmt.Sprintf("%x", randBytes)
+
+		private, err := crypto.HexToECDSA(os.Getenv("VALIST_SIGNER"))
+		if err != nil {
+			return accounts.Account{}, err
+		}
+
+		// when VALIST_SIGNER is set, this encrypted key is stored in an os.TempDir()
+		// when program halts, the ephemeral encryption key is cleared, making the keystore file inaccessible
+		// this prevents having to clear the keystore file during runtime, which could fail to execute if program is interrupted/crashes
+		// @TODO emulate filesystem in-memory when using VALIST_SIGNER to remove need for this workaround
+		account, err = cfg.KeyStore().ImportECDSA(private, passphrase)
+		if err != nil {
+			return accounts.Account{}, err
+		}
+
+		c.Context = context.WithValue(c.Context, passphraseKey, passphrase)
+	}
+
+	return account, nil
+}
+
+func UnlockAccount(c *cli.Context) error {
+	client := c.Context.Value(core.ClientKey).(*client.Client)
+
+	// check for ephemeral encryption key (will be set if using VALIST_SIGNER env var)
+	passphrase := c.Context.Value(passphraseKey).(string)
+
+	if passphrase == "" {
+		pass, err := prompt.AccountPassphrase().RunFlag(c, passphraseKey)
+		if err != nil {
+			return err
+		}
+		passphrase = pass
+	}
+
+	err := client.Signer().Unlock(client.Account(), passphrase)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SetupClient(c *cli.Context) error {
+	cfg, err := loadConfig()
+	if err != nil {
+		return err
+	}
+
+	account, err := getAccount(c, cfg)
+	if err != nil {
+		return err
 	}
 
 	client, err := core.NewClient(c.Context, cfg, account)
@@ -47,54 +121,9 @@ func SetupClient(c *cli.Context) error {
 }
 
 func SetupClientWithPassphrase(c *cli.Context) error {
-	home, err := os.UserHomeDir()
-	if err != nil {
+	if err := SetupClient(c); err != nil {
 		return err
 	}
 
-	if err := config.Initialize(home); err != nil {
-		return err
-	}
-
-	cfg := config.NewConfig(home)
-	if err := cfg.Load(); err != nil {
-		return err
-	}
-
-	var account accounts.Account
-	if c.IsSet("account") {
-		account.Address = common.HexToAddress(c.String("account"))
-	} else {
-		account.Address = cfg.Accounts.Default
-	}
-
-	passphrase, err := prompt.AccountPassphrase().RunFlag(c, "passphrase")
-	if err != nil {
-		return err
-	}
-
-	client, err := core.NewClientWithPassphrase(c.Context, cfg, account, passphrase)
-	if err != nil {
-		return err
-	}
-
-	c.Context = context.WithValue(c.Context, core.ClientKey, client)
-	c.Context = context.WithValue(c.Context, core.ConfigKey, cfg)
-	return nil
-}
-
-func UnlockAccount(c *cli.Context) error {
-	client := c.Context.Value(core.ClientKey).(*client.Client)
-
-	passphrase, err := prompt.AccountPassphrase().RunFlag(c, "passphrase")
-	if err != nil {
-		return err
-	}
-
-	err = client.Signer().Unlock(client.Account(), passphrase)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return UnlockAccount(c)
 }
