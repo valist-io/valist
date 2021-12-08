5e1f87abb53e6b9bc1a400cd11c0c06c3c3270ed
Alec M. Wantoch <alec@wantoch.com>
2021-12-08 13:16:59 -0500

    tweak local ipfs not found error

--- storage/ipfs/node.go
+++ storage/ipfs/node.go
@@ -57,7 +57,7 @@ func newIPFS(ctx context.Context, repoPath string) (coreiface.CoreAPI, error) {
 		return local, nil
 	}
 
-	fmt.Println("WARNING: failed to connect to local IPFS")
+	fmt.Println("Local IPFS node not found, using embedded node instead.")
 	once.Do(setupPlugins)
 
 	if err := initRepo(repoPath); err != nil {
