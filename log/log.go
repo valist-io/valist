35a4e98305595e8476b198a4dbbaa0fb8249c006
Keenan Nemetz <keenan.nemetz@gmail.com>
2021-12-23 14:31:30 -0600

    add emoji. add stdout and stderr loggers

--- log/log.go
+++ log/log.go
@@ -7,22 +7,36 @@ import (
 	"github.com/fatih/color"
 )
 
+var (
+	warnEmoji   = "⚠️  "
+	errorEmoji  = "⛔ "
+	noticeEmoji = "✨ "
+)
+
 type Logger struct {
-	log *log.Logger
+	out *log.Logger
+	err *log.Logger
 }
 
 func New() *Logger {
-	return &Logger{log.New(os.Stdout, "", 0)}
+	return &Logger{
+		out: log.New(os.Stdout, "", 0),
+		err: log.New(os.Stderr, "", 0),
+	}
 }
 
 func (l *Logger) Info(msg string, args ...interface{}) {
-	l.log.Printf(msg, args...)
+	l.out.Printf(msg, args...)
+}
+
+func (l *Logger) Notice(msg string, args ...interface{}) {
+	l.out.Printf("%s%s", noticeEmoji, color.CyanString(msg, args...))
 }
 
 func (l *Logger) Warn(msg string, args ...interface{}) {
-	l.log.Println(color.YellowString(msg, args...))
+	l.err.Printf("%s%s", warnEmoji, color.YellowString(msg, args...))
 }
 
 func (l *Logger) Error(msg string, args ...interface{}) {
-	l.log.Println(color.RedString(msg, args...))
+	l.err.Printf("%s%s", errorEmoji, color.RedString(msg, args...))
 }
