ee992786553009198967dd968b40367466d3b9bb
Keenan Nemetz <keenan.nemetz@gmail.com>
2022-03-23 10:25:09 -0700

    mount ipfs data directory in docker-compose

--- docker-compose.sh
+++ docker-compose.sh
@@ -8,12 +8,4 @@ docker_pid=$!
 
 trap "docker-compose down" SIGINT SIGABRT
 
-while docker-compose exec ipfs cat /data/ipfs/api; [[ $? -eq 1 ]]; do sleep 1; done
-
-docker-compose exec ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
-
-docker-compose exec ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "[\"PUT\", \"GET\", \"POST\"]"
-
-docker-compose exec ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"
-
 wait $docker_pid
